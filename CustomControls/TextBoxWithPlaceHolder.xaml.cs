2452a8d8cdabd426c8d5109751094f58e736c399
Jd's Code Lab <jdscodelab@gmail.com>
2021-04-07 14:13:21 +0530

    Add files via upload
    - Initial Commit

---
+++ CustomControls/TextBoxWithPlaceHolder.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginUI.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for TextBoxWithPlaceHolder.xaml
+    /// </summary>
+    public partial class TextBoxWithPlaceHolder : UserControl
+    {
+        public TextBoxWithPlaceHolder()
+        {
+            InitializeComponent();
+        }
+
+        public string PlaceHolder
+        {
+            get { return (string)GetValue(PlaceHolderProperty); }
+            set { SetValue(PlaceHolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PlaceHolder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(string), typeof(TextBoxWithPlaceHolder));
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextBoxWithPlaceHolder));
+
+
+
+
+        public bool IsPassword
+        {
+            get { return (bool)GetValue(IsPasswordProperty); }
+            set { SetValue(IsPasswordProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsPassword.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPasswordProperty =
+            DependencyProperty.Register("IsPassword", typeof(bool), typeof(TextBoxWithPlaceHolder));
+
+        private void passbox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            email.Text = passbox.Password;
+        }
+    }
+}
---
+++ Pages/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        //Show another page on login button click
+        MainWindow mainWindow { get => Application.Current.MainWindow as MainWindow; }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.mainFrame.Navigate(new Uri("/pages/dashboard.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
